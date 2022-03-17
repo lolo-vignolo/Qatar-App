766f1472540b016079b716018fee4cc434375791
vignolo_3@hotmail.com <vignolo_3@hotmail.com>
2022-03-17 22:17:45 +0100

    env variables

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,11 +1,15 @@
+
+
+
+
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-keys.json");
+
 
 //para evitar que se inicie de nuevo y de error lo hago con try.
 try{
   admin.initializeApp({
-    credential: admin.credential.cert(process.env.password),
+    credential: admin.credential.cert(process.env.JSONKEY),
     databaseURL: "https://qatar-app-3478a.firebaseio.com" 
   });
 }catch (e) { console.log(e);}
